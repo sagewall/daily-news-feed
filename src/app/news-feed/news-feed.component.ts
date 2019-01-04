26d704349296255a89c219862263c6241c6d4c94
Sage Wall <sagewall@users.noreply.github.com>
2019-01-03 17:50:26 -0700

    Update to angular cli version 7.1.4

--- src/app/news-feed/news-feed.component.ts
+++ src/app/news-feed/news-feed.component.ts
@@ -9,7 +9,7 @@ import { Source } from '../source';
 @Component({
   selector: 'app-news-feed',
   templateUrl: './news-feed.component.html',
-  styleUrls: ['./news-feed.component.sass']
+  styleUrls: ['./news-feed.component.scss']
 })
 export class NewsFeedComponent implements OnInit {
 
