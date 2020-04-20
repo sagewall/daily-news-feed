0339b3b042b410ddbea445b86a0d6f2c24b031f7
Sage Wall <sagewall@users.noreply.github.com>
2020-04-20 10:46:18 -0600

    Add search component stub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -14,9 +16,10 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { NewsFeedComponent } from './news-feed/news-feed.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, NewsFeedComponent],
+  declarations: [AppComponent, NewsFeedComponent, SearchComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -25,7 +28,9 @@ import { NewsFeedComponent } from './news-feed/news-feed.component';
     LayoutModule,
     MatButtonModule,
     MatCardModule,
+    MatFormFieldModule,
     MatIconModule,
+    MatInputModule,
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
